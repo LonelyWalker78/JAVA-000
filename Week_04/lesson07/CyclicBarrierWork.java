cc24cf735c6e396470aad028a602d5bdb9181f1a
walke78 <13625987765@163.com>
2020-11-11 21:40:29 +0800

    week04 作业提交

---
+++ Week_04/lesson07/CyclicBarrierWork.java
@@ -0,0 +1,56 @@
+
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+
+public class CyclicBarrierWork {
+
+    private volatile Integer value = null;
+    CyclicBarrier barrier;
+
+    public void setBarrier(CyclicBarrier barrier) {
+        this.barrier = barrier;
+    }
+
+    public void sum(int num) throws BrokenBarrierException, InterruptedException {
+        value = fibo(num);
+        barrier.await();
+    }
+
+    private int fibo(int a) {
+        return a;
+    }
+
+    public int getValue() throws InterruptedException {
+        return value;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        long start=System.currentTimeMillis();
+
+        final CyclicBarrierWork method = new CyclicBarrierWork();
+        CyclicBarrier barrier = new CyclicBarrier(1, ()-> {
+            int result = 0;
+            try {
+                result = method.getValue();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            System.out.println("异步计算结果为："+result);
+            System.out.println("使用时间："+ (System.currentTimeMillis()-start) + " ms");
+        });
+        method.setBarrier(barrier);
+
+        Thread thread = new Thread(() -> {
+            try {
+                method.sum(100);
+            } catch (BrokenBarrierException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+        thread.start();
+    }
+
+}
---
+++ Week_04/lesson07/SemaphoreWork.java
@@ -0,0 +1,45 @@
+
+import java.util.concurrent.Semaphore;
+
+public class SemaphoreWork {
+
+    private Integer value = null;
+    private final Semaphore semaphore = new Semaphore(1);
+
+    public void sum(int num) throws InterruptedException {
+        semaphore.acquire();
+        value = fibo(num);
+        System.out.println(value);
+        semaphore.release();
+    }
+
+    private int fibo(int a) {
+        return a;
+    }
+
+    public int getValue() throws InterruptedException {
+        int result;
+        semaphore.acquire();
+        result = this.value;
+        semaphore.release();
+        return result;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        long start=System.currentTimeMillis();
+        final SemaphoreWork method = new SemaphoreWork();
+        Thread thread = new Thread(() -> {
+            try {
+                method.sum(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+        thread.start();
+        thread.join();
+        int result = method.getValue(); //这是得到的返回值
+        System.out.println("异步计算结果为："+result);
+        System.out.println("使用时间："+ (System.currentTimeMillis()-start) + " ms");
+    }
+
+}
