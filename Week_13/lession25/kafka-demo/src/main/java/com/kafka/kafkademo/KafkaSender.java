3063f589a73c8789bfc3874ad963d37a4bf251d1
lincj <13625987765@163.com>
2021-01-13 23:43:21 +0800

    week13作业提交

---
+++ Week_13/lession25/kafka-demo/src/main/java/com/kafka/kafkademo/KafkaSender.java
@@ -0,0 +1,23 @@
+package com.kafka.kafkademo;
+
+import com.alibaba.fastjson.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class KafkaSender {
+
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    //发送消息方法
+    public void send(int i) {
+        JSONObject message = new JSONObject();
+        message.put("time", System.currentTimeMillis());
+        message.put("msg", "第"+i + "条消息");
+        message.put("price", "hhhh");
+        kafkaTemplate.send("topic", message.toJSONString());
+    }
+
+}
